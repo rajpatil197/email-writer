f643cbfa18a50fbc9aae9e711ff50bb13572bfcb
rajpatil197 <patilrajvardhan420@gmail.com>
2026-08-07 20:35:21 +0530

    Email Generator Completed

--- email-writer/src/main/java/com/email/writer/Controllers/EmailGeneratorController.java
+++ email-writer/src/main/java/com/email/writer/Controllers/EmailGeneratorController.java
@@ -6,13 +6,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
+@CrossOrigin(origins = "http://localhost:4200")
 @AllArgsConstructor
 public class EmailGeneratorController {
 
